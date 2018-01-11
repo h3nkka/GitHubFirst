7a67a7f0bec7f17e24820232ec9ac3f7bd15e4b0
K8948 <K8948@student.jamk.fi>
2018-01-11 17:06:01 +0200

    Now we have 10 Hello GITTYs

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -10,7 +10,10 @@ namespace GitHub
     {
         static void Main(string[] args)
         {
-
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Hello GITTY! " + i + "\n");
+            }
         }
     }
 }
